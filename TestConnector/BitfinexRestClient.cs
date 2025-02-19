c5e9ebb62b93e40bdd56c94a6cddc46c634444b7
Nikita Smolianitski <n.smolianitski@gmail.com>
2025-02-19 14:23:53 +0300

    Add BitfinexRestClient GetCandleSeriesAsync method

--- TestConnector/BitfinexRestClient.cs
+++ TestConnector/BitfinexRestClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
+using Microsoft.AspNetCore.WebUtilities;
 using TestConnector.HQTestData;
 
 namespace TestConnector;
@@ -45,5 +46,75 @@ public class BitfinexRestClient
             });
 
         return trades;
+    }
+
+    public async Task<IEnumerable<Candle>> GetCandleSeriesAsync(
+        string pair,
+        int periodInSec,
+        DateTimeOffset? from = null,
+        DateTimeOffset? to = null,
+        long? count = 0)
+    {
+        var queryParams = new Dictionary<string, string?>
+        {
+            {"start", from?.ToUnixTimeMilliseconds().ToString()},
+            {"end", to?.ToUnixTimeMilliseconds().ToString()},
+            {"limit", count.ToString()},
+        };
+        var interval = GetClosestInterval(periodInSec);
+        var url = QueryHelpers
+            .AddQueryString($"{BaseAddress}/candles/trade:{interval}:t{pair}/hist", queryParams);
+        
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        using var response = await _httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var responseString = await response.Content.ReadAsStringAsync();
+        var result = JsonSerializer.Deserialize<List<List<JsonElement>>>(responseString);
+        if (result == null)
+            throw new InvalidOperationException($"Failed to deserialize response. Response string: {responseString}");
+
+        var candles = result.Select(elements =>
+        {
+            var openPrice = elements[1].GetDecimal();
+            var closePrice = elements[2].GetDecimal();
+            var highPrice = elements[3].GetDecimal();
+            var lowPrice = elements[4].GetDecimal();
+            var totalVolume = elements[5].GetDecimal();
+            var totalPrice = (openPrice + closePrice + highPrice + lowPrice) / 4m * totalVolume;
+            return new Candle
+            {
+                Pair = pair,
+                OpenTime = DateTimeOffset.FromUnixTimeMilliseconds(elements[0].GetInt64()),
+                OpenPrice = openPrice,
+                ClosePrice = closePrice,
+                HighPrice = highPrice,
+                LowPrice = lowPrice,
+                TotalVolume = totalVolume,
+                TotalPrice = totalPrice
+            };
+        });
+
+        return candles;
+    }
+
+    private string GetClosestInterval(int periodInSec)
+    {
+        var intervalData = new (int Seconds, string Interval)[]
+        {
+            (60, "1m"), (300, "5m"), (900, "15m"), (1800, "30m"),
+            (3600, "1h"), (10800, "3h"), (21600, "6h"), (43200, "12h"),
+            (86400, "1D"), (604800, "1W"), (1209600, "14D"), (2592000, "1M")
+        };
+
+        foreach (var (sec, interval) in intervalData)
+        {
+            if (periodInSec <= sec)
+                return interval;
+        }
+        
+        return intervalData[^1].Interval;
     }
 }
